44e9a6386afcf78d74a17b7a943f2f32244dbf47
hieuit <info.hieuit@gmail.com>
2017-07-18 13:59:01 +0700

    init project bai 1

---
+++ Bai1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bai1.Startup))]
+namespace Bai1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
